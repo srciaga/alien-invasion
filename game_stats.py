a0a81293336011d5d56feeed6c9f649b02c4fa55
srciaga <srciaga@gmail.com>
2022-01-05 15:58:30 -0800

    High score is saved after closing game

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,6 @@
+import json
+
+
 class GameStats:
     """Track statistics for Alien Invasion."""
 
@@ -6,11 +9,19 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # Start in an inactive state.
+        # Start game in an inactive state.
         self.game_active = False
 
         # High score should never be reset.
-        self.high_score = 0
+        self.high_score = self.get_saved_high_score()
+
+    def get_saved_high_score(self):
+        """Gets high score from file, if it exists."""
+        try:
+            with open('data/high_score.json') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
